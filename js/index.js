42d8e208e665864523e71c259ae7494dfde58a59
Vitaliy <klimov_vitaliy@icloud.com>
2020-10-08 19:28:38 +0300

    added some actions: new class and toggle it

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 (function () {
 
-    const select = document.querySelector('.select');
+    const select = document.querySelector('.select'),
+          selectArea = document.querySelector('.select__area'),
+          selectList = document.querySelector('.select__list'),
+          selectItems = document.getElementsByClassName('select__item');
 
-    function selectList () {
-        return ``;
+
+    selectArea.addEventListener('click', oppenSelect);
+
+    for (selectItem of selectItems) {
+        selectItem.addEventListener('click', oppenSelect)
+    }
+
+    function oppenSelect () {
+        selectList.classList.toggle('select__list--opened');
     }
 
 
