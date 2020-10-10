74f58031155356f68c64c7f18cbf552e825cf7ec
Vitaliy <klimov_vitaliy@icloud.com>
2020-10-11 00:06:00 +0300

    Added multy-select

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
 (function () {
 
-    const select = document.querySelector('.select'),
-          selectArea = document.querySelector('.select__area'),
-          selectList = document.querySelector('.select__list'),
-          selectItems = document.getElementsByClassName('select__item');
-
+    const select        = document.querySelector('.select'),
+          selectArea    = document.querySelector('.select__area'),
+          selectList    = document.querySelector('.select__list'),
+          selectItems   = document.querySelectorAll('.select__item'),
+          selectedItem  = document.querySelector('.selected__item'),
+          deleteItemBtn = document.querySelector('.deleteItemBtn');
 
     selectArea.addEventListener('click', oppenSelect);
 
     for (selectItem of selectItems) {
-        selectItem.addEventListener('click', oppenSelect)
+        selectItem.addEventListener('click', addItem)
     }
-
-    function oppenSelect () {
+    
+    function oppenSelect (e) {
         selectList.classList.toggle('select__list--opened');
     }
 
+    function addItem (e) {
+        (select.hasAttribute('data-multy') && 
+            (selectArea.innerHTML += item(e.target.innerHTML))) || 
+            (selectArea.innerHTML = e.target.innerHTML)
+        
+        oppenSelect();
+    }
+
+    function item (value) {
+        return `<div class="selected__item">
+                    ${value} 
+                    <button class="deleteItemBtn">&#10006;</button>
+                </div>`
+    }
 
 })();
