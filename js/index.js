741e84fa39b18598b64903876245ae59e7b0e7ff
Vitaliy <klimov_vitaliy@icloud.com>
2020-12-30 07:48:25 +0200

    fetching data

--- js/index.js
+++ js/index.js
@@ -1,13 +1,48 @@
 (function () {
 
-    const select        = document.querySelector('.select'),
+    const body          = document.querySelector('body'),
+          select        = document.querySelector('.select'),
           selectArea    = document.querySelector('.select__area'),
-          selectList    = document.querySelector('.select__list'),
           selectItems   = document.querySelectorAll('.select__item'),
           selectedItem  = document.querySelector('.selected__item'),
           deleteItemBtn = document.querySelector('.deleteItemBtn');
 
+    let selectList = document.querySelector('.select__list')
+
+    const dataUrl = './js/data.json'
+
+    async function fetchData(url) {
+        try {
+            const response = await fetch(url)
+            const data = response.json()
+
+            return data;
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    let data = fetchData(dataUrl)
+                    .then(data => {
+                        return data; 
+                    })
+    console.log(data);
+
     selectArea.addEventListener('click', oppenSelect);
+
+    function initial(data) {
+        data.forEach((obj) => {
+            let li = document.createElement("li")
+
+            li.classList.add('select__item')
+            li.innerHTML = obj.name
+
+            console.log(li);
+            selectList.push(li)
+        })
+    }
+
 
     for (selectItem of selectItems) {
         selectItem.addEventListener('click', addItem)
@@ -25,11 +60,11 @@
         oppenSelect();
     }
 
-    function item (value) {
-        return `<div class="selected__item">
-                    ${value} 
-                    <button class="deleteItemBtn">&#10006;</button>
-                </div>`
-    }
+    // function item (value) {
+    //     return `<div class="selected__item">
+    //                 ${value} 
+    //                 <button class="deleteItemBtn">&#10006;</button>
+    //             </div>`
+    // }
 
 })();
